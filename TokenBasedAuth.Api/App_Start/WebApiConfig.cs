ba7b4f0b732e3cf149bd663f1189f2d1cd18e79e
Luis Pool <Luis_Pool@epam.com>
2017-05-25 13:28:48 -0500

    * Add missing files.

--- TokenBasedAuth.Api/App_Start/WebApiConfig.cs
+++ TokenBasedAuth.Api/App_Start/WebApiConfig.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
-
-namespace TokenBasedAuth.Api
+﻿namespace TokenBasedAuth.Api
 {
+    using Microsoft.Owin.Security.OAuth;
+    using System.Web.Http;
+
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
